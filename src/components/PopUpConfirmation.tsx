4ae85d7af26c7547c79451b3c05499e2379927e0
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-08-02 00:12:00 +0530

    🔥 Write css

--- src/components/PopUpConfirmation.tsx
+++ src/components/PopUpConfirmation.tsx
@@ -10,14 +10,14 @@ const ConfirmationModal = (
     onCancel 
   }: Props) => {
   return (
-    <div className="confirmation-modal">
-      <div className="confirmation-modal-content">
-        <p>{message}</p>
-        <div className="confirmation-modal-actions">
-          <button onClick={onConfirm} className="confirm-button">
+    <div className=" fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-50">
+      <div className="h-1/4 w-1/3 bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-5 rounded-md flex flex-col items-center justify-between">
+        <p className=" text-2xl font-semibold">{message}</p>
+        <div className="flex flex-row gap-4 w-full">
+          <button onClick={onConfirm} className="text-white text-xl font-medium w-full border py-2 rounded-lg bg-red-600 hover:bg-red-700">
             Yes
           </button>
-          <button onClick={onCancel} className="cancel-button">
+          <button onClick={onCancel} className="text-white text-xl font-medium w-full border py-2 rounded-lg bg-green-500  hover:bg-green-600">
             No
           </button>
         </div>
